f95b49612b008dccba33332784b17c6a698fcafb
cassiemeow <37783670+cassiemeow@users.noreply.github.com>
2020-05-03 15:48:57 -0700

    Update 3.r

--- 3.r
+++ 3.r
@@ -5,24 +5,79 @@ train = read.csv("train.data.csv")
 test = read.csv("test.data.csv")
 
 ### (a)
+
 # R2 indicates the percentage of the variance in the dependent variable that the independent variables explain collectively.
 
-train.house = lm(price ~ bedrooms + bathrooms + sqft_living + sqft_lot, data = train)
-summary(train.house)$r.squared # 0.5101139
+train.house = lm(price ~ bedrooms+bathrooms+sqft_living+sqft_lot, data = train)
+# summary(train.house)$r.squared # 0.5101139
 
-test.house = lm(price ~  bedrooms + bathrooms + sqft_living + sqft_lot, data = test)
-summary(test.house)$r.squared # 0.5054477 
+# test.house = lm(price ~ bedrooms+bathrooms+sqft_living+sqft_lot, data = test)
+# summary(test.house)$r.squared # 0.5054477
+
+train.X <- as.matrix(cbind(rep(1, nrow(train)), apply(train[,5:8],2,scale)))
+train.y <- as.matrix(train[,4])
+fit <- lm(train.y ~ train.X[,-1])
+
+test.X <- as.matrix(cbind(rep(1, nrow(test)), apply(test[,5:8],2,scale)))
+test.y <- as.matrix(test[,4])
+
+pred.y <- test.X %*% matrix(fit$coefficients, ncol = 1, nrow = ncol(test.X))
+   
+# function to calculate R-square
+rsquare <- function(obs, fitted) {
+  r2 = 1 - (sum((obs-fitted)^2)) / (sum((obs-mean(obs))^2))
+  return(r2)
+}
+ 
+train.r2 <- rsquare(train$price, fit$fitted.values) # 0.5101139
+test.r2 <- rsquare(test.y, pred.y) # 0.5049332
+
+out <- cbind(train.r2, test.r2) %>% as.data.frame()
+colnames(out) <- c("Training Data", "Test Data")
+rownames(out) <- "R-square Value"
+
+knitr::kable(out, align = "c") %>%
+  kable_styling(bootstrap_options = "striped", full_width = F)
+
+
 
 ### (b)
-fit.house     = lm(price ~ bedrooms + bathrooms + sqft_living + sqft_lot, data = house)
-predict(fit.house, fancy)
+fit.house = lm(price ~ bedrooms+bathrooms+sqft_living+sqft_lot, data = house)
+pred <- predict(fit.house, fancy) # 15,390,168
+
+out <- cbind("15,390,168", "127,000,000") %>% as.data.frame()
+colnames(out) <- c("Predicted Value", "Real Value")
+rownames(out) <- "Bill Gate's House"
+
+knitr::kable(out, align = "c") %>%
+  kable_styling(bootstrap_options = "striped", full_width = F)
+
+
 
 ### (c)
-train.house.2 = lm(price ~ sqft_living + sqft_lot + bedrooms*bathrooms, data = train)
-summary(train.house.2)$r.squared # 0.5173533
+train.house.2 = lm(price ~ sqft_living+sqft_lot+bedrooms*bathrooms, data = train)
+summary(train.house.2)
 
-test.house.2  = lm(price ~ sqft_living + sqft_lot + bedrooms*bathrooms, data = test)
-summary(test.house.2)$r.squared # 0.5110569
+# test.house.2 = lm(price ~ sqft_living+sqft_lot+bedrooms*bathrooms, data = test)
+# summary(test.house.2)$r.squared # 0.5110569
+
+fit.2 <- lm(train.y ~ train.X[,2]*train.X[,3]+train.X[,4]+train.X[,5])
+test.X.2 <- as.matrix(cbind(test.X, test.X[,2]*test.X[,3]))
+
+pred.y.2 <- test.X.2 %*% matrix(fit.2$coefficients, ncol = 1, nrow = ncol(test.X.2))
+
+train.r2.2 <- rsquare(train$price, fit.2$fitted.values) # 0.5173533
+test.r2.2 <- rsquare(test.y, pred.y.2) # 0.5105277
+
+out <- cbind(train.r2.2, test.r2.2) %>% as.data.frame()
+colnames(out) <- c("Training Data", "Test Data")
+rownames(out) <- "R-square Value"
+
+knitr::kable(out, align = "c") %>%
+  kable_styling(bootstrap_options = "striped", full_width = F)
+
+
+
 
 ### (d)
 
