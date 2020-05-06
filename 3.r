ae747d6f707a7053ab40753ac9b5fc8029213799
cassiemeow <37783670+cassiemeow@users.noreply.github.com>
2020-05-06 16:15:05 -0700

    Update 3.r

--- 3.r
+++ 3.r
@@ -91,7 +91,28 @@ checkStrict <- function(f, silent=FALSE) {
 }
 
 
-gda <- function(data, eps = 0.001, max.iter = 50, standardize = T, seed=123) {
+##################### For linear model fit with no interaction term:
+dat.lm = train[,c(5:8,4)]
+dat.lm.scale = scale(dat.lm)
+y = dat.lm.scale[,ncol(dat.lm.scale)]
+X = cbind(1, dat.lm.scale[,-ncol(dat.lm.scale)])
+
+theta_opt_lm = summary(lm(y ~ X[,2]+X[,3]+X[,4]+X[,5]))$coefficients[,1]
+
+##################### For linear model fit with interaction term:
+dat.lm.int = cbind(train[c(5:8)], train[5]*train[6], train[4])
+colnames(dat.lm.int) = c("bedrooms", "bathrooms", "sqft_living","sqft_lot",
+                         "interaction", "price")
+dat.lm.int.scale = scale(dat.lm.int)
+y = dat.lm.int.scale[,ncol(dat.lm.int.scale)]
+X = cbind(1, dat.lm.int.scale[,-ncol(dat.lm.int.scale)])
+
+theta_opt_lm.int = summary(lm(y ~ X[,2]+X[,3]+X[,4]+X[,5]+X[,6]))$coefficients[,1]
+
+
+##################### Create a Gradient Descent Function:
+gda <- function(data, eps = 0.001, max.iter = 50, standardize = T, seed=123,
+                optimal.theta) {
   
   set.seed(seed)
   #scaling data 
@@ -104,11 +125,9 @@ gda <- function(data, eps = 0.001, max.iter = 50, standardize = T, seed=123) {
   X <- cbind(X0 = 1, data[,-p]) # add a column of 1 to serve as the intercept
   y <- data[,p]
   
-  #oracke solution(after passing the test, can be removed)
-  theta_opt = summary(lm(y ~ X[,2] + X[,3] + X[,4] + X[,5]))$coefficients[,1]
-  
   #starting values of theta
-  theta <- matrix(runif(n = p), ncol = p, nrow=1)
+  theta <- matrix(runif(n = p), 
+                  ncol = p, nrow=1)
   theta.new <- theta
   
   ###gradient descent###
@@ -119,66 +138,77 @@ gda <- function(data, eps = 0.001, max.iter = 50, standardize = T, seed=123) {
   
   #iteration
   step <- 1
-  while ( step <= max.iter & norm(theta_opt - theta) > eps ) { 
+  while ( step <= max.iter & norm(optimal.theta - theta) > eps ) { 
     theta = theta.new
     res <- (X %*% t(theta)) - y
     gradient = t(res) %*% X
     theta.new = theta - gradient * stepsize
     step <- step + 1
-    # print(norm(theta_opt - theta))
+    print(norm(optimal.theta - theta))
   }
   return(theta)
 }
 
-data = train[c("bedrooms", "bathrooms", "sqft_living", "sqft_lot", "price")]
-GD.out <- gda(data, standardize = TRUE)
-# gda(data, standardize = FALSE)
+GD.out <- gda(train[,c(5:8,4)], standardize = TRUE, optimal.theta = theta_opt_lm)
+
+GD.out.int <- gda(dat.lm.int, 
+                  standardize = TRUE, optimal.theta = theta_opt_lm.int,
+                  max.iter = 500)
 
 ## R-square for training data
-X.prep <- scale(train[,c(5:8,4)])
-X <- cbind(X0 = 1, X.prep[,-ncol(X.prep)])
+get.r2 <- function (train = train[,c(5:8,4)],
+                    test = test[,c(5:8,4)], SGD_result) {
+  ## R-square for training data
+  X.prep <- scale(train)
+  X <- cbind(X0 = 1, X.prep[,-ncol(X.prep)])
+  pred.y <- X %*% SGD_result[1,]
+  r2.train <- rsquare(X.prep[,5], pred.y)
 
-pred.y <- X %*% GD.out[1,]
-r2.train <- rsquare(X.prep[,5], pred.y) # 0.5101116 VERY CLOSE!
+  ## R-square for test data
+  X.prep <- scale(test)
+  X <- cbind(X0 = 1, X.prep[,-ncol(X.prep)])
+  pred.y <- X %*% SGD_result[1,]
+  r2.test <- rsquare(X.prep[,5], pred.y)
 
-## R-square for test data
-X.prep <- scale(test[,c(5:8,4)])
-X <- cbind(X0 = 1, X.prep[,-ncol(X.prep)])
+  return(c(r2.train,r2.test))
+}
 
-pred.y <- X %*% GD.out[1,]
-r2.test <- rsquare(X.prep[,5], pred.y) # 0.5052524
+dat.lm.int.test <- cbind(test[,c(5:8)],test[,5]*test[,6],test[,4])
+
+r2.lm = get.r2(train[,c(5:8,4)], test[,c(5:8,4)], GD.out)
+r2.lm.int = get.r2(dat.lm.int, dat.lm.int.test, GD.out.int)
 
 
 ## make a table for comparison
-out <- cbind(c(train.r2, test.r2),c(r2.train, r2.test)) %>% as.data.frame()
-colnames(out) <- c("lm Fit", "Gradient Descent")
+out <- cbind(c(train.r2, test.r2),c(r2.lm[1], r2.lm[2]), 
+             c(train.r2.2, test.r2.2),c(r2.lm.int[1],r2.lm.int[2])) %>% as.data.frame()
+colnames(out) <- c("lm Fit", "Gradient Descent - lm", 
+                   "lm with Interaction Fit", "Gradient Descent - lm+int")
 rownames(out) <- c("Training Data", "Test Data")
 
 knitr::kable(out, align = "c", caption = "R-square Comparison") %>%
   kable_styling(bootstrap_options = "striped", full_width = F)
 
 
+
 ### (e)
 
 sgda <- function(data, max.iter = 30, stepsize = 5,
-                 standardize = T, seed=123) {
+                 standardize = T, seed=123, optimal = theta_opt_lm.int) {
 
   set.seed(seed)
   #scaling data
   data <- as.matrix(data)
   p = ncol(data)
   n = nrow(data)
-  if(standardize) {data <- scale(data)} # scale data if required
-
-  # #oracke solution(after passing the test, can be removed)
-  # theta_opt = summary(lm(y ~ X[,2] + X[,3] + X[,4] + X[,5]))$coefficients[,1]
+  if (standardize) { data <- scale(data) } # scale data if required
 
   #predictor and response
   X <- cbind(X0 = 1, data[,-p]) # add a column of 1 to serve as the intercept
   y <- data[,p]
 
   #starting values of theta
-  theta <- matrix(runif(n = p), ncol = p, nrow=1)
+  theta <- matrix(runif(n = p), ncol = p, nrow = 1)
 
   #iteration
   j <- 0
@@ -195,34 +225,26 @@ sgda <- function(data, max.iter = 30, stepsize = 5,
     gradient <- t(res) %*% t(X.new)
     theta <- theta - gradient * eta
     ref <- norm(gradient,"2")
-    # print(norm(theta_opt - theta))
+    print(norm(optimal - theta))
   }
   # theta <- y * theta/X
   return(theta)
 }
 
-SGD.out <- sgda(train[,c(5:8,4)], standardize = T, stepsize = 1, max.iter = 1000)
-# gda(data, standardize = FALSE)
+# SGD.out <- sgda(train[,c(5:8,4)], standardize = T, stepsize = 1, max.iter = 1000)
+sgda(dat.lm.int, standardize = T, stepsize = 1, max.iter = 100)
 
-get.r2 <- function (SGD_result) {
-  ## R-square for training data
-  X.prep <- scale(train[,c(5:8,4)])
-  X <- cbind(X0 = 1, X.prep[,-ncol(X.prep)])
-  pred.y <- X %*% SGD_result[1,]
-  r2.train <- rsquare(X.prep[,5], pred.y)
 
-  ## R-square for test data
-  X.prep <- scale(test[,c(5:8,4)])
-  X <- cbind(X0 = 1, X.prep[,-ncol(X.prep)])
-  pred.y <- X %*% SGD_result[1,]
-  r2.test <- rsquare(X.prep[,5], pred.y)
+output <- sapply(seq(1,10,1), 
+                 FUN = function(x) get.r2(train[,c(5:8,4)],test[,c(5:8,4)],
+                                          sgda(train[,c(5:8,4)], standardize = T, 
+                                               stepsize = x, max.iter = 2000)))
 
-  return(c(r2.train,r2.test))
-}
+# bibi <- sapply(seq(1,10,1), FUN = function(x) get.r2(int.data, int.data.test,
+#   SGD_result = sgda(dat.lm.int, standardize = T, stepsize = x, max.iter = 100)))
 
-# output <- sapply(seq(1,10,1), FUN = function(x) get.r2(sgda(train[,c(5:8,4)], standardize = T, stepsize = x, max.iter = 2000)))
-# save(output, file = ".../output.rda")
-load(".../output.rda")
+# save(output, file = "/Users/xuchenghuiyun/Desktop/STA243/data/output.rda")
+# load("/Users/xuchenghuiyun/Desktop/STA243/data/output.rda")
 
 
 ## select stepsize
